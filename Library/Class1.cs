81b8696aa1c249d11828d4487d82cec7454ebca7
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-07-14 14:13:51 +0300

    Created class User in Library Project

---
+++ Library/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Library
+{
+    public class User
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public User(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public double GetSalary(double salaryPerHour, int workTime) => salaryPerHour * workTime;
+    }
+}
